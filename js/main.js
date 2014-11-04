7682dad946b12635f1c56d7e4088e91d39342a42
Matheus Carmo <me@matheuslc.com>
2014-11-04 01:34:56 -0200

    Menu, function that actives the responsive menu

--- js/main.js
+++ js/main.js
@@ -4,3 +4,32 @@
  */
 
 
+;(function (document, window) {
+  'use strict';
+
+  // App namespace
+
+  var APP = {} || window.APP;
+
+  /* Active functions
+   *
+   */
+  APP.init = function() {
+    APP.toggleMenu('.anchor');
+  };
+
+  /* Active responsive menu
+   * @param anchor {string} Anchor elemnt class
+   */
+   APP.toggleMenu = function(anchor) {
+      var el = document.querySelector(anchor);
+      document.querySelector('body')
+              .classList
+              .toggleClass('menu-active');
+   };
+
+   window.addEventListener('load', function() {
+    APP.init();
+   });
+
+}(window, document));
